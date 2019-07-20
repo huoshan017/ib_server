b38992966f38f5f8283d3d14f562999906ce7171
huoshan017 <huoshan017@github.com>
2019-07-21 04:21:11 +0800

    11111

--- bat/gen_db.bat
+++ bat/gen_db.bat
@@ -1,13 +1,5 @@
 set GOPATH=%cd%/../../../../..
 
-cd ../conf/db
-md proto
-
-cd ../../tools
-code_generator.exe -c ../conf/db/game_db.json -d ../src/game -p ../conf/db/proto/game_db.proto
-code_generator.exe -c ../conf/db/account_db.json -d ../src/account -p ../conf/db/proto/account_db.proto
-
-cd protobuf
-protoc.exe --go_out=../../src/game/game_db --proto_path=../../conf/db/proto game_db.proto
-protoc.exe --go_out=../../src/account/account_db --proto_path=../../conf/db/proto account_db.proto
-cd ../../proxy/test
+cd ../tools
+code_generator.exe -c ../conf/db/game_db.json -d ../src/game -p protobuf/protoc.exe
+code_generator.exe -c ../conf/db/account_db.json -d ../src/account -p protobuf/protoc.exe
